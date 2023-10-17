1502329de458f4bdf0cfac8b27d9563564fb8753
Akshay <apm@gmail.com>
2023-10-17 20:54:14 +0530

    books list showing successfully

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express, { request } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
+const app = express();
+
+app.use("/books", booksRoute);
+
+// middleware for parsing request body
+app.use(express.json());
+
+// middleware for handling CORS policy
+// option 1 : allow all origin with default of CORS(*)
+app.use(cors());
+// option 2 : Allow custom origins
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     method: ["GET", "PUT", "POST", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("response is sent");
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to mongoose");
+    app.listen(PORT, () => console.log(`app is listening to port ${PORT}`));
+  })
+  .catch((error) => {
+    console.log("mongoose error");
+  });
